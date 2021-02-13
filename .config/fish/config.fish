bb0623e971250510f2fbbdf2276fb021a308caea
GGKapanadze <kapanadzeg41@gmail.com>
2021-02-13 12:58:10 +0400

    🔧 Remove llm alias.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,7 +12,6 @@ alias yt='ytop -c monokai'
 alias l='exa -lbF --git'                                               # list, size, type, git
 alias ls='exa'                                                         # ls
 alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGd --git --sort=modified'                            # long list, modified date sort
 alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
 alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
 
