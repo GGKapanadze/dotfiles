2c38470b412a7dfb8d118c254082f7d71c8565f9
GGKapanadze <doublegkapanadze@gmail.com>
2024-01-12 20:31:52 +0400

    concifg: updated fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,35 @@
 set fish_greeting
 
 # Bindings
-bind \cR 'peco_history'
+bind \cr peco_history # Bind for peco select history to Ctrl+R
+bind \cf peco_directory # Bind for peco change directory to Ctrl+F
+bind \cg lazygit # Bind for lazygit to Ctrl+G
 
 # Aliases
 alias go=z
-alias c='xclip -selection clipboard'
+alias c='pbcopy'
 alias yt='ytop -c monokai'
+alias ls="lsd"
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
 alias vim='nvim'
-alias ls='exa'
-alias ll='exa -lga --icons'
-alias lx='exa -lga --icons --level=2 --tree'
 
 
 starship init fish | source
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
+zoxide init fish | source
+
+# if status is-interactive
+#     # Commands to run in interactive sessions can go here
+# end
+
+# pnpm
+set -gx PNPM_HOME "/Users/ggkapanadze/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+set --universal nvm_default_version v20.10.0
+set -gx XDG_CONFIG_HOME $HOME/.config
